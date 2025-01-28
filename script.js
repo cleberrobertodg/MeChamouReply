ee49d47e37e4bdc4289f193829c459d8f29a8542
cleberrobertodg <cleberrobertodg@gmail.com>
2025-01-28 10:44:36 -0300

    alteração de planos, mensal / anual

--- script.js
+++ script.js
@@ -6,9 +6,23 @@ document.addEventListener('DOMContentLoaded', function () {
         if (this.checked) {
             anual.style.color = '#3e66f3'
             mensal.style.color = 'black'
+            document.querySelector("#bottom-card > div.card-content > div > h3").textContent = 'PREMIUM'
+            document.querySelector("#bottom-card > div.card-content > div > h2").textContent =  'Plano Premium'
+            document.querySelector("#price > h2").textContent = 'R$299'
+            document.querySelector("#bottom-card > span").textContent = 'Ideal para empresas de grande porte'
+            document.querySelectorAll("#check-area > div.check-text > span").forEach(function(elemento) {
+                elemento.textContent = '10 Usuários';
+            })
         } else {
             anual.style.color = 'black'
             mensal.style.color = '#3e66f3'
+            document.querySelector("#bottom-card > div.card-content > div > h3").textContent = 'BASIC'
+            document.querySelector("#bottom-card > div.card-content > div > h2").textContent =  'Plano Básico'
+            document.querySelector("#price > h2").textContent = 'R$29'
+            document.querySelector("#bottom-card > span").textContent = 'Ideal para quem está começando'
+            document.querySelectorAll("#check-area > div.check-text > span").forEach(function(elemento) {
+                elemento.textContent = '1 Usuário';
+            })
         }
     })
 })

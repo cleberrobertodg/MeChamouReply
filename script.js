f9750284656ee03472f093e11a57cd7fa4369461
cleberrobertodg <cleberrobertodg@gmail.com>
2025-01-28 11:41:16 -0300

    Adição de animação de transição entre a troca de planos

--- script.js
+++ script.js
@@ -1,28 +1,60 @@
 document.addEventListener('DOMContentLoaded', function () {
-    document.querySelector("#toggle-area > div > label > input[type=checkbox]").addEventListener('change', function () {
+    const toggle = document.querySelector("#toggle-area > div > label > input[type=checkbox]");
+    const bottomCard = document.querySelector('#bottom-card');
+    const bottomCard2 = document.querySelector('#bottom-card2');
+    
+    
+    function fadeIn(element) {
+        element.style.opacity = 0;
+        element.style.display = 'flex'; 
+
+        let opacity = 0;
+        let fadeInterval = setInterval(function() {
+            opacity += 0.05;
+            element.style.opacity = opacity;
+
+            if (opacity >= 1) {
+                clearInterval(fadeInterval);
+            }
+        }, 30); 
+    }
+
+    
+    function fadeOut(element) {
+        let opacity = 1;
+        let fadeInterval = setInterval(function() {
+            opacity -= 0.05;
+            element.style.opacity = opacity;
+
+            if (opacity <= 0) {
+                clearInterval(fadeInterval);
+                element.style.display = 'none'; 
+            }
+        }, 30); 
+    }
+
+    toggle.addEventListener('change', function () {
         const anual = document.querySelector('.anual')
         const mensal = document.querySelector('.mensal')
         
         if (this.checked) {
             anual.style.color = '#3e66f3'
             mensal.style.color = 'black'
-            document.querySelector("#bottom-card > div.card-content > div > h3").textContent = 'PREMIUM'
-            document.querySelector("#bottom-card > div.card-content > div > h2").textContent =  'Plano Premium'
-            document.querySelector("#price > h2").textContent = 'R$299'
-            document.querySelector("#bottom-card > span").textContent = 'Ideal para empresas de grande porte'
-            document.querySelectorAll("#check-area > div.check-text > span").forEach(function(elemento) {
-                elemento.textContent = '10 Usuários';
-            })
+            
+            
+            fadeOut(bottomCard);
+            setTimeout(function() {
+                fadeIn(bottomCard2);
+            }, 500); 
         } else {
             anual.style.color = 'black'
             mensal.style.color = '#3e66f3'
-            document.querySelector("#bottom-card > div.card-content > div > h3").textContent = 'BASIC'
-            document.querySelector("#bottom-card > div.card-content > div > h2").textContent =  'Plano Básico'
-            document.querySelector("#price > h2").textContent = 'R$29'
-            document.querySelector("#bottom-card > span").textContent = 'Ideal para quem está começando'
-            document.querySelectorAll("#check-area > div.check-text > span").forEach(function(elemento) {
-                elemento.textContent = '1 Usuário';
-            })
+            
+            
+            fadeOut(bottomCard2);
+            setTimeout(function() {
+                fadeIn(bottomCard);
+            }, 500); 
         }
-    })
-})
+    });
+});
